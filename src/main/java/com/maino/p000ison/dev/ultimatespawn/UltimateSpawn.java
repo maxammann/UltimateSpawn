0190b21bea7b5d9aa6d943aca2b2d73524287150
p000ison <monstermax.ammann@googlemail.com>
2012-04-15 17:30:09 +0200

    Inital Commit

---
+++ src/main/java/com/maino/p000ison/dev/ultimatespawn/UltimateSpawn.java
@@ -0,0 +1,12 @@
+package com.maino.p000ison.dev.ultimatespawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author p00ison
+ * @author maino
+ */
+public class UltimateSpawn extends JavaPlugin {
+    
+}
